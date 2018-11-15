4a516c8b2bff06722af4fb56ade3a99f194cad9f
Shielarose <shielarosegandhi@gmail.com>
2018-11-14 22:35:01 -0500

    played around with js

--- js/main.js
+++ js/main.js
@@ -6,44 +6,71 @@ app.ctx = app.canvas.getContext("2d");
 app.canvas.width = 500;
 app.canvas.height = 500;
 
+app.ctx.imageSmoothingEnabled = false;
+
+// Array of images
+app.images = {};
+app.images.blueDino = new Image();
+app.images.blueDino = "./assets/dinoCharacters/blueDino.png";
+app.images.redDino = new Image();
+app.images.redDino = "./assets/dinoCharacters/redDino.gif";
+app.images.yellowDino = new Image(); 
+app.images.yellowDino = "./assets/dinoCharacters/yellowDino.gif"; 
+app.images.greenDino = new Image();
+app.images.greenDino = "./assets/dinoCharacters/greenDino.gif";
+app.images.heart = new Image();
+app.images.heart = ".assets/heart.png";
+app.images.steak = new Image();
+app.images.steak = ".assets/Steak.png";
+app.images.meteor = new Image();
+app.images.meteor = ".assets/meteor.png";
+
+
 // Add dino to canvas 
 // Original Position of Dino
-app.rectX = 210;
-app.rectY = app.canvas.height - 100;
+app.dinoPosX = app.canvas.width / 2 - 40;
+app.dinoPosY = app.canvas.height - 100;
 
 app.image = new Image();
-app.image.src = "./assets/dinoCharacters/dino_mort/0.gif";
-app.chosenDino = app.ctx.drawImage(app.image, app.rectX, app.rectY, 80, 80);
+app.image.src = app.images.redDino;
+app.chosenDino = app.ctx.drawImage(app.image, app.dinoPosX, app.dinoPosY, 80, 80);
+
+app.init = function(){
+   app.move;
+};
 
 // Make dino move right and left using arrow keys
 app.move = function(e) {
-   if (e.keyCode == 39) { 
-      app.rectX+=15; 
+   if(e.keyCode == 39) { 
+      app.dinoPosX+=15; 
    } //right arrow
    
    if(e.keyCode == 37) { 
-      app.rectX-=15; 
+      app.dinoPosX-=15; 
    } //left arrow
 
-
 // Dino cannot go out of bounds
-   if(app.rectX < app.rectX / 5) {
-      app.rectX = app.rectX / 5;
+   if(app.dinoPosX < 0) {
+      app.dinoPosX = 0;
    }
 
-   if(app.rectX > app.canvas.width - (app.rectX / 5)) {
-      app.rectX = app.canvas.width - (app.rectX / 5);
+   if(app.dinoPosX > app.canvas.width - 80) {
+      app.dinoPosX = app.canvas.width - 80;
    }
 
    app.ctx.clearRect(0, 0, app.canvas.width, app.canvas.height);
-   app.chosenDino = app.ctx.drawImage(app.image, app.rectX, app.rectY, 80, 80);
+   app.chosenDino = app.ctx.drawImage(app.image, app.dinoPosX, app.dinoPosY, 80, 80);
 };
 
-
-
-
-
 document.onkeydown = app.move;
+
+
+// Steaks fall out of top of canvas 
+
+
+$(function(){
+   app.init();
+});
 
 // Pop-up comes up on load on top of the game asking the player to choose their dino and select start
 
