33caa5ef4e09e8d54d7bc3d5477b3f6113c01d40
Shielarose <shielarosegandhi@gmail.com>
2018-11-15 12:02:41 -0500

    tried to get steak image to show up

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ app.canvas.height = 500;
 
 app.ctx.imageSmoothingEnabled = false;
 
+
 // Array of images
 app.images = {};
 app.images.blueDino = new Image();
@@ -19,53 +20,130 @@ app.images.yellowDino = "./assets/dinoCharacters/yellowDino.gif";
 app.images.greenDino = new Image();
 app.images.greenDino = "./assets/dinoCharacters/greenDino.gif";
 app.images.heart = new Image();
-app.images.heart = ".assets/heart.png";
+app.images.heart = "./assets/heart.png";
 app.images.steak = new Image();
-app.images.steak = ".assets/Steak.png";
+app.images.steak = "./assets/Steak.png";
 app.images.meteor = new Image();
-app.images.meteor = ".assets/meteor.png";
+app.images.meteor = "./assets/meteor.png";
 
 
 // Add dino to canvas 
-// Original Position of Dino
-app.dinoPosX = app.canvas.width / 2 - 40;
-app.dinoPosY = app.canvas.height - 100;
-
-app.image = new Image();
-app.image.src = app.images.redDino;
-app.chosenDino = app.ctx.drawImage(app.image, app.dinoPosX, app.dinoPosY, 80, 80);
-
-app.init = function(){
-   app.move;
+app.charactersAndItems = {};
+// Player
+app.charactersAndItems.dino = {
+   name: "Blue",
+   x: app.canvas.width / 2 - 40,
+   y: app.canvas.height - 80,
+   spdX: 15,
+   spdY: 0,
+   hp: 4,
+   width: 80,
+   height: 80,
 };
 
-// Make dino move right and left using arrow keys
-app.move = function(e) {
-   if(e.keyCode == 39) { 
-      app.dinoPosX+=15; 
-   } //right arrow
-   
-   if(e.keyCode == 37) { 
-      app.dinoPosX-=15; 
-   } //left arrow
+// Draw player
+app.playerImage = new Image();
+app.playerImage.src = app.images.redDino;
+app.chosenDino = app.ctx.drawImage(app.playerImage, app.charactersAndItems.dino.x, app.charactersAndItems.dino.y, app.charactersAndItems.dino.width, app.charactersAndItems.dino.height);
 
-// Dino cannot go out of bounds
-   if(app.dinoPosX < 0) {
-      app.dinoPosX = 0;
+// Move dino with arrow keys
+app.charactersAndItems.moveDino = function(e){
+   if (e.keyCode == 39) {
+      app.charactersAndItems.dino.x += app.charactersAndItems.dino.spdX;
+   } // right arrow
+
+   if (e.keyCode == 37) {
+      app.charactersAndItems.dino.x -= app.charactersAndItems.dino.spdX;
+   } // left arrow
+
+   // Dino cannot go out of bounds
+   if (app.charactersAndItems.dino.x < 0) {
+      app.charactersAndItems.dino.x = 0;
    }
 
-   if(app.dinoPosX > app.canvas.width - 80) {
-      app.dinoPosX = app.canvas.width - 80;
+   if (app.charactersAndItems.dino.x > app.canvas.width - 80) {
+      app.charactersAndItems.dino.x = app.canvas.width - 80;
    }
 
    app.ctx.clearRect(0, 0, app.canvas.width, app.canvas.height);
-   app.chosenDino = app.ctx.drawImage(app.image, app.dinoPosX, app.dinoPosY, 80, 80);
+   app.chosenDino = app.ctx.drawImage(app.playerImage, app.charactersAndItems.dino.x, app.charactersAndItems.dino.y, app.charactersAndItems.dino.width, app.charactersAndItems.dino.height);
 };
+document.onkeydown = app.charactersAndItems.moveDino;
 
-document.onkeydown = app.move;
+// Steaks
+app.charactersAndItems.steak = {
+   name: "Steak",
+   x: app.canvas.width / 2 - 40,
+   y: app.canvas.height - 250,
+   spdX: 0,
+   spdY: 15,
+   width: 80,
+   height: 80,
+}
+
+// Draw Steaks
+app.steakImage = new Image();
+app.steakImage.src = app.images.steak;
+app.steak = app.ctx2.drawImage(app.steakImage, app.charactersAndItems.steak.x, app.charactersAndItems.steak.y, app.charactersAndItems.steak.width, app.charactersAndItems.steak.height);
 
 
-// Steaks fall out of top of canvas 
+// app.init = function(){
+//    app.charactersAndItems.move;
+// };
+
+// Make dino move right and left using arrow keys
+// app.moveDino = function(e){
+//    if(e.keyCode == 39) {
+//       app.dino.posX += app.dino.spdX;
+//    } // right arrow
+
+//    if(e.keyCode == 37) {
+//       app.dino.posX -= app.dino.spdX;
+//    } // left arrow
+
+//    // Dino cannot go out of bounds
+//    if(app.dino.posX < 0) {
+//       app.dino.posX = 0;
+//    }
+
+//    if(app.dino.posX > app.canvas.width - 80) {
+//       app.dino.posX = app.canvas.width - 80;
+//    }
+
+//    app.ctx.clearRect(0, 0, app.canvas.width, app.canvas.height);
+//    app.chosenDino = app.ctx.drawImage(app.image, app.dino.posX, app.dino.posY, 80, 80);
+// };
+
+
+
+// // Steaks fall out of top of canvas 
+// // Steaks
+// app.steak = {
+//    posX: 250,
+//    posY: 250,
+//    spdX: 0,
+//    spdY: 10,
+// };
+
+
+// app.steak.posX += app.steak.spdX;
+// app.steak.posY += app.steak.spdY;
+// app.steak.image = new Image();
+// app.steak.image.src = app.images.steak;
+// app.ctx.drawImage(app.steak.image, app.steak.posX, app.steak.posY);
+
+
+
+
+// // Meteors
+// app.meteor = {
+//    posX: 150,
+//    posY: 150,
+//    spdX: 0,
+//    spdY: 15,
+// };
+
+
 
 
 $(function(){
